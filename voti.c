58b8dfab9f3e9ced50f3162c521cd2d202f34e71
Matteo Enut <sirtoeteo@gmail.com>
2022-10-11 20:43:52 +0200

    updated voti

--- voti.c
+++ voti.c
@@ -6,23 +6,50 @@
 int main()
 {
     int voti[N];
-    int i, j;
+    int i, j, max;
+    char c;
+
+    /* randomizza gli elementi */
 
     for (i = 0; i < N; i++)
     {
         voti[i] = rand() % 10 + 1;
     }
 
+    max = voti[0];
+
+    /* trova il massimo */
+
     for (i = 0; i < N; i++)
     {
-        printf("%d ", voti[i]);
+        max = (voti[i] > max) ? voti[i] : max;
+    }
 
-        for (j = 0; j < voti[i]; j++)
+    /* stampa gli asterischi dall'alto verso il basso*/
+
+    for (i = max; i > 0; i--)
+    {
+        for (j = 0; j < N; j++)
         {
-            printf("* ");
+            c = (voti[j] >= i) ? '*' : ' ';
+            printf("%c  ", c);
         }
 
         printf("\n");
+    }
+
+    for (i = 0; i < N * 3; i++)
+    {
+        printf("-");
+    }
+
+    printf("\n");
+
+    /* stampa i numeri */
+
+    for (i = 0; i < N; i++)
+    {
+        printf("%-2d ", voti[i]);
     }
 
     return 0;
